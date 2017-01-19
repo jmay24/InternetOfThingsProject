5603f1bc2607a872891276efae391fedc3adcfcc
jmay24 <Jokiemay1>
2017-01-19 15:35:25 +0000

    currently working but listing too much in list view

--- app/src/main/java/com/m/j/project/josephmay/Chat.java
+++ app/src/main/java/com/m/j/project/josephmay/Chat.java
@@ -8,15 +8,24 @@ public class Chat {
     private String mName;
     private String mMessage;
     private String mUid;
+    private Float mHumi;
+    private Float mTemp;
 
     public Chat() {
         // Needed for Firebase
     }
 
-    public Chat(String name, String message, String uid) {
+  /*  public Chat(String name, String message, String uid, float Humi, float Temp) {
         mName = name;
         mMessage = message;
         mUid = uid;
+        mHumi = Humi;
+        mTemp = Temp;
+    }*/
+
+    public Chat(float Humi, float Temp) {
+        mHumi = Humi;
+        mTemp = Temp;
     }
 
     public String getName() {
@@ -34,6 +43,14 @@ public class Chat {
     public void setMessage(String message) {
         mMessage = message;
     }
+
+    public float  getHumi(){ return mHumi; }
+
+    public void setHumi (float Humi) { mHumi = Humi; }
+
+    public float  getTemp(){ return mTemp; }
+
+    public void setTemp (float Temp) { mTemp = Temp; }
 
     public String getUid() {
         return mUid;
