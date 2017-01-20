df50149ba71a3dcd21bc8193db48da763406e76b
jmay24 <Jokiemay1>
2017-01-20 11:47:48 +0000

    made class for old code. cleaned up main activity.

--- app/src/main/java/com/m/j/project/josephmay/MainActivity.java
+++ app/src/main/java/com/m/j/project/josephmay/MainActivity.java
@@ -4,42 +4,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseReference ref;
-    ListView listview;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listview = (ListView) findViewById(R.id.listview);
 
-
-
-        final ListView listview = (ListView) findViewById(R.id.listview);
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-
-        Chat msg = new Chat(23 , 66);
-        ref.push().setValue(msg);
-
-
-        //DatabaseReference chatRef = ref.child("chatMessage");
-        //Query recentMessages = chatRef.limitToLast(5);
-
+/*
         final FirebaseListAdapter<Chat> mAdapter = new FirebaseListAdapter<Chat>(this, Chat.class, android.R.layout.two_line_list_item, ref) {
             @Override
             protected void populateView(View view, Chat chatMessage, int position) {
@@ -51,15 +35,25 @@ public class MainActivity extends AppCompatActivity {
         };
 
         listview.setAdapter(mAdapter);
+*/
 
+        final ArrayAdapter<Chat> adapter = new ArrayAdapter<Chat>(this,android.R.layout.simple_dropdown_item_1line);
 
-        ref.limitToLast(1).addValueEventListener(new ValueEventListener() {
+        listview.setAdapter(adapter);
+
+        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        Chat sendData = new Chat(23 , 66);
+        ref.push().setValue(sendData);
+
+        ref.limitToLast(2).addValueEventListener(new ValueEventListener() {
         @Override
         public void onDataChange(DataSnapshot snapshot) {
 
+
             for (DataSnapshot msgSnapshot: snapshot.getChildren()) {
                 Chat msg = msgSnapshot.getValue(Chat.class);
-                Log.i("Chat", msg.getName()+": " +msg.getMessage());
+                adapter.clear();
+                adapter.add(msg);
             }
         }
         @Override
