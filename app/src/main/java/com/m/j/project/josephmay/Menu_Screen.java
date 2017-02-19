4928faba00fd8c19cae4b99bb62cf53237c96297
jmay24 <jokiemay@live.co.uk>
2017-02-19 21:05:58 +0000

    updated view for temp and humi added png files

--- app/src/main/java/com/m/j/project/josephmay/Menu_Screen.java
+++ app/src/main/java/com/m/j/project/josephmay/Menu_Screen.java
@@ -1,8 +1,18 @@
 package com.m.j.project.josephmay;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Joseph on 19/02/2017.
  */
 
-public class Menu_Screen {
+public class Menu_Screen extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+    }
 }
