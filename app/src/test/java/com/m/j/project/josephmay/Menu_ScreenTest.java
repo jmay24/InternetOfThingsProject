5afc414cc47c95cc6acea97a81c9156ec0600666
jmay24 <jokiemay@live.co.uk>
2017-04-16 07:55:03 +0100

    updated UI

---
+++ app/src/test/java/com/m/j/project/josephmay/Menu_ScreenTest.java
@@ -0,0 +1,26 @@
+package com.m.j.project.josephmay;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joseph on 10/04/2017.
+ */
+public class Menu_ScreenTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+
+    }
+
+}
